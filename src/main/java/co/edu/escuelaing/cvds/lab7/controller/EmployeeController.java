f5656e8204ad7bdbc48bc8752749177c201c0336
SebSanUwU <juancamargo206@gmail.com>
2023-10-30 15:54:57 -0500

    Avance de los charts Parte II

--- src/main/java/co/edu/escuelaing/cvds/lab7/controller/EmployeeController.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/controller/EmployeeController.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 import java.util.List;
 
 @Controller
@@ -55,14 +59,63 @@ public class EmployeeController {
     }
 
     @GetMapping("/analitica")
-    public String mostrarAnaliticaEmpleados(Model model){
-        model.addAttribute("empleados",employeeService.getAllEmployees());
+    public String getCharts(Model model) {
+        List<Employee> empleados = employeeService.getAllEmployees();
+
+        Map<String, ArrayList<Employee>> companyData = new TreeMap<>();
+        for(Employee empleado:empleados){
+            if(!companyData.containsKey(empleado.getCompany())){
+                ArrayList<Employee> empleadosList = new ArrayList<>();
+                empleadosList.add(empleado);
+                companyData.put(empleado.getCompany(),empleadosList);
+            }else{
+                companyData.get(empleado.getCompany()).add(empleado);
+            }
+        }
+
+        //Persona por sexo en la base de datos
+        int female=0;
+        int male=0;
+
+        for(Employee empleado:empleados){
+            if (empleado.getSex().equals("Female")){
+                female++;
+            }else {
+                male++;
+            }
+        }
+
+        Map<String, Integer> graphData = new TreeMap<>();
+        graphData.put("Male", male);
+        graphData.put("Female", female);
+        model.addAttribute("chartData", graphData);
+
+        //Número de empleados por empresa
+        Map<String, Integer> graphDataCompanyEmployeesCount = new TreeMap<>();
+        companyData.forEach((key, value) -> {
+
+            //System.out.println("Clave: " + key + ", Valor: " + value.size());
+            Integer theInt= value.size();
+            graphDataCompanyEmployeesCount.put(key, theInt);
+
+        });
+        model.addAttribute("pieCountData", graphDataCompanyEmployeesCount);
+
+        //Salario promedio por empresa
+        Map<String, Float> graphDataCompanySalaryProm = new TreeMap<>();
+        companyData.forEach((key, value) -> {
+            final float[] aux = {0};
+            value.forEach(empleado -> {
+                aux[0] +=empleado.getSalary();
+            });
+
+            //System.out.println("Clave: " + key + ", Salario Prom: " + aux[0]/ value.size());
+            graphDataCompanySalaryProm.put(key,aux[0]/ value.size());
+        });
+        model.addAttribute("salaryPromData", graphDataCompanySalaryProm);
+
         return "analitica";
     }
 
-    @GetMapping("/api")
-    public List<Employee> obtenerTodosLosEmpleados() {
-        return employeeService.getAllEmployees();
-    }
 
 }
